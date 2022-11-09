33f983de8ca4b462188dd6309e64230f477d8257
Julian Cardone <juliancardone1@gmail.com>
2022-11-09 14:57:46 -0500

    handleclick

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -2,22 +2,26 @@ import { API, broadcast } from "./util";
 
 export default class FollowToggle {
   constructor(toggleButton) {
-    // this.toggleButton = toggleButton
-
+    this.toggleButton = toggleButton
+    toggleButton.addEventListener('click', this.handleClick.bind(this))
     // toggleButton.setAttribute("data-user-id", "")
   }
 
 
   async handleClick(event) {
     // Your code here
+    event.preventDefault()
+    this.toggleButton.getAttribute('data-follow-state') === 'followed' ? this.unfollow() : this.follow()
   }
 
   async follow() {
     // Your code here
+    
   }
 
   async unfollow() {
     // Your code here
+    
   }
 
   render() {
